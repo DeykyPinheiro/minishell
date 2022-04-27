13dbef6c4e02f741e5a44cb21d6e760525a359fd
deykypinheiro <pinheiromikael96@gmail.com>
2022-04-27 02:31:56 -0300

    style: colored path

--- src/minishell.c
+++ src/minishell.c
@@ -14,6 +14,36 @@ int	is_valid_line(t_minishell *shell)
 	return (is_valid);
 }
 
+char *colorir_cwd(char *cwd)
+{
+	char *tmp;
+	char *colored;
+
+	tmp = ft_strjoin(CYAN, cwd);
+	free(cwd);
+	colored = ft_strjoin(tmp, DEFAULT"\n" RED("❯ "));
+	free(tmp);
+	return (colored);
+}
+
+char	*get_current_path()
+{
+	char	*cwd;
+
+	cwd = ft_calloc(PATH_MAX, sizeof(char));
+	if(!cwd)
+	{
+		printf("aviso de erro\n");
+		return (NULL);
+	}
+	if(!getcwd(cwd, PATH_MAX))
+	{
+		printf("aviso de erro\n");
+		return (NULL);
+	}
+	return(colorir_cwd(cwd));
+}
+
 void	minishell(int argc, char *argv[], char *envp[])
 {
 	(void)argc;
@@ -21,11 +51,12 @@ void	minishell(int argc, char *argv[], char *envp[])
 	(void)envp;
 
 	t_minishell	shell;
-
-	printf("estou dentro do src \n");
+	char		*cwd;
 	while (1)
 	{
-		shell.line = readline("IMPRIMIR ALGO BONITO AQUI\n");
+		cwd = get_current_path();
+		shell.line = readline(cwd);
+		free(cwd);
 		if (!shell.line)
 			EXIT_SUCCESS;
 		if (!is_valid_line(&shell))
--- src/minishell.h
+++ src/minishell.h
@@ -2,9 +2,17 @@
 # define MINISHELL_H
 
 #include <stdio.h>
+#include <unistd.h>
 # include "../libraries/libft/src/libft.h"
 # include <readline/readline.h>
 # include <readline/history.h>
+
+ #define PATH_MAX 4096
+
+# define CYAN "\001\033[0;36m\002"
+# define DEFAULT "\001\e[0;39m\002"
+# define BLUE(string) "\x1b[34m" string "\x1b[0m"
+# define RED(string) "\x1b[31m" string "\x1b[0m"
 
 typedef struct s_minishell
 {
