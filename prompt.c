77dab129d85039e8ab868d4fa147117f75a36906
deykypinheiro <pinheiromikael96@gmail.com>
2022-05-22 22:17:47 -0300

    feat: symtab

--- prompt.c
+++ prompt.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include "shell.h"
+#include "symtab/symtab.h"
+
 
 // imprime "$ " para poder receber o comando
 void print_prompt1(void)
 {
-	fprintf(stderr, "$ ");
+	struct symtab_entry_s *entry = get_symtab_entry("PS1");
+
+	if (entry && entry->val)
+	{
+		fprintf(stderr, "%s", entry->val);
+	}
+	else
+	{
+		fprintf(stderr, "$ ");
+	}
 }
 
 // imprime "> " para poder receber o comando
 void print_prompt2(void)
 {
-	fprintf(stderr, "> ");
+	struct symtab_entry_s *entry = get_symtab_entry("PS2");
+
+	if(entry && entry->val)
+	{
+		fprintf(stderr, "%s", entry->val);
+	}
+	else
+	{
+		fprintf(stderr, "> ");
+	}
 }
--- shell.h
+++ shell.h
@@ -8,4 +8,16 @@ void	print_prompt2(void);
 // receber o comando da CLI
 char	*read_cmd(void);
 
+int dump(int argc, char **argv);
+
+struct builtin_s
+{
+    char *name;
+    int (*func)(int argc, char **argv);
+};
+
+extern struct builtin_s builtins[];
+
+extern int builtins_count;
+
 #endif
